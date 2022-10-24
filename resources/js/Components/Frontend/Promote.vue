9b211ae0bd588b90f3daa69fbdb8a4e86ec2ecb3
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-10-24 15:15:25 +0200

    Add mockup styling to welcome page

--- resources/js/Components/Frontend/Promote.vue
+++ resources/js/Components/Frontend/Promote.vue
@@ -22,15 +22,15 @@
     <div class="carousel-inner">
         <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
         <div class="carousel-item">
-            <img src="http://fakeimg.pl/2000x800/0079D8/fff/?text=Without">
+            <img src="../../../../public/images/Img_Portfolio/Wireframe and Mockup.png" class="mx-[35%]">
         </div>
         <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
         <div class="carousel-item">
-            <img src="http://fakeimg.pl/2000x800/DA5930/fff/?text=JavaScript">
+            <img src="../../../../public/images/Img_Portfolio/Atrium Mockup.png" class="mx-[40%]">
         </div>
         <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
         <div class="carousel-item">
-            <img src="http://fakeimg.pl/2000x800/F90/fff/?text=Carousel">
+            <img src="../../../../public/images/Img_Portfolio/About.jpg" class="mx-[30%]">
         </div>
         <label for="carousel-3" class="carousel-control prev control-1">‹</label>
         <label for="carousel-2" class="carousel-control next control-1">›</label>
--- resources/js/Components/Frontend/Skills.vue
+++ resources/js/Components/Frontend/Skills.vue
@@ -7,7 +7,7 @@ defineProps({
 <template>
      <section class="bg-lightGreen py-12">
         <div container mx-auto>
-            <div class="grid grid-cols-8 md:grid-flow-col">
+            <div class="grid grid-cols-9 md:grid-flow-col">
                 <div class="flex items-center justify-center" v-for="skill in skills.data" :key="skill.id">
                     <img :src="skill.image" :alt="skill.name" class="w-[25%]">
                 </div>
