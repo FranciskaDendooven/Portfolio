456c63924bb13edb31f4d20083554add8421a463
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-10-31 16:56:49 +0100

    Fix text animation for mobile screen size

--- resources/js/Components/Frontend/Hero.vue
+++ resources/js/Components/Frontend/Hero.vue
@@ -7,15 +7,15 @@
 
     <section id="home" class="flex flex-wrap bg-lightBlack items-center lg:py-0 overflow-hidden">
         <div class="container pb-12 mx-auto h-full">
-            <div class="flex flex-col px-32 md:flex-row mx-auto h-full pt-8">
+            <div class="flex flex-col px-32 lg:px-32 md:flex-row mx-auto h-full pt-8">
                 <div class="flex-1 flex flex-col items-start pt-8 lg:items-start"> 
 
-                    <TextAnimationName></TextAnimationName>
+                    <TextAnimationName class="w-full sm:w-[100px]"></TextAnimationName>
 
-                    <p id="text-grows-and-stayes-big" class="pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] font-body text-sm text-center lg:text-left">TRY - FAIL - LEARN - <b id="lightGreen-text-grows-and-stayes-big">TRY AGAIN</b>
+                    <p id="text-grows-and-stayes-big" class="pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] font-body text-sm lg:text-left">TRY - FAIL - LEARN - <b id="lightGreen-text-grows-and-stayes-big">TRY AGAIN</b>
                     </p>
-                    <p class="pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] font-body text-lg text-center lg:text-left">
-                    Want to know more? Check out About & Skills <br> by clicking the button below.
+                    <p class="pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] font-body text-lg lg:text-left">
+                    Want to know more? Check out About & Skills by clicking the button below.
                     </p>
                     <button type="" class="btn btn-md bg-lightGreen text-lightBlack font-bold py-1 px-2 rounded-full hover:bg-lightDarkGreen hover:text-lightBeige md:btn-lg transition-all">
                         <a href="#about">Push Me</a>
